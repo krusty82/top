a2fea957769287c7c3cc58c066e61cbb02929682
krusty82 <git@weier2000.de>
2026-06-27 22:18:43 +0200

    Hillshade optimize

--- scripts/update_tiles.sh
+++ scripts/update_tiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 osm2pgsql-replication \
     update -d gis \
-    --max-diff-size 10  --  \
+    --max-diff-size 100  --  \
     -G \
     -C 3000 --number-processes 4 \
-    --flat-nodes /mnt/db/flat-nodes/gis-flat-nodes.bin  \
+    --flat-nodes /mnt/1tb/tablespace/flat-nodes/gis-flat-nodes.bin  \
     --style /home/otm/db/opentopomap.style \
     --expire-tiles=11-17 \
-    --expire-output=/dirty_tiles.txt
+    --expire-output=/mnt/dirty_tiles.txt
